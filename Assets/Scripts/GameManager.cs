bc557cfdbdeb256b65583f5908f79b1e150ac0b7
TNK2718 <mizuki2718@ruri.waseda.jp>
2021-01-28 02:12:14 +0900

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    //テストとしてレベルを3にしておく
+    private int level = 3;
+
+    //Awake : Sceneを移動した時即座に実行される
+    void Awake () {
+        if(instance == null){
+            instance = this;
+        } else if(instance != this){
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        
+        //BoardManager取得
+        boardScript = GetComponent<BoardManager> ();
+        InitGame ();
+    }
+
+    void InitGame () {
+        //BoardManagerのSetupSceneメソッドを実行
+        boardScript.SetupScene (level);
+    }
+}
